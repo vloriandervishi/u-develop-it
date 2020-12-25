2fa36721bc0adb44ba5bdebfe37a6c2210310223
Valdo <vlorian22@GMAIL.COM>
2020-12-25 16:59:31 -0600

    added parties table and inserted more members into schema.sql and seeds.sql ran migrate and seeds in the ternminal;

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,15 @@
+
+DROP TABLE IF EXISTS candidates;
+DROP TABLE IF EXISTS parties;
 CREATE TABLE candidates (
   id INTEGER PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
   industry_connected BOOLEAN NOT NULL
+);
+
+CREATE TABLE parties (
+  id INTEGER PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  description TEXT
 );
