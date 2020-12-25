2081f31c90c75dda7e701362167f5a9b027dfec9
Valdo <vlorian22@GMAIL.COM>
2020-12-25 17:29:06 -0600

    We've added a new line to the table called-constraint. This allows us to flag the party_id field as an official foreign key and tells the SQL which table and field it references. In this case, it references the id filed in the paries table. This ensures thant no id can be inserted into the candidates table if it doesntt also exist in the arties table. SQLITE will return an error for any operation that would violate a constraint.

--- db/schema.sql
+++ db/schema.sql
@@ -5,7 +5,9 @@ CREATE TABLE candidates (
   id INTEGER PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  industry_connected BOOLEAN NOT NULL
+  industry_connected BOOLEAN NOT NULL,
+  party_id iNTEGER UNSIGNED,
+  CONSTRAINT fk_party FOREIGN KEY (party_id)REFERENCES parties(id) ON DELETE  SET NULL
 );
 
 CREATE TABLE parties (
